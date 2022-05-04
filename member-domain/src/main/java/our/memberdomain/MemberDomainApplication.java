ef13e6ec0c8705da160e8304be0ee0197c27806c
tis <92219795+this-is-spear@users.noreply.github.com>
2022-05-04 15:31:11 +0900

    docs : README 생성

---
+++ member-domain/src/main/java/our/memberdomain/MemberDomainApplication.java
@@ -0,0 +1,13 @@
+package our.memberdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberDomainApplication.class, args);
+	}
+
+}
---
+++ member-domain/src/test/java/our/memberdomain/MemberDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package our.memberdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
