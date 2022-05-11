dcbd1af67056f950d426c625c2c07ae4b99ff8d2
tis <92219795+this-is-spear@users.noreply.github.com>
2022-05-11 09:50:50 +0900

    feat : 스프링 프로젝트 구성

---
+++ src/test/java/our/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package our.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
